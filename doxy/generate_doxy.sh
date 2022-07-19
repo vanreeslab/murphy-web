e33c58531be8d10a44312cd9d1390933770adf7d
Wim van Rees <wvanrees@mit.edu>
2022-07-19 15:08:48 -0700

    adds comments to the doxybook generate script

--- doxy/generate_doxy.sh
+++ doxy/generate_doxy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# update these two paths according to your own machine
 MURPHY_DOC_DIR="/Users/wvanrees/software/murphy/doc/"
 DOXYBOOK2_EXEC="../doxybook2-osx-amd64-v1.4.0/bin/doxybook2"
 
+# leave this one be
 DOXYBOOK2_OUT_DIR="../docs/doxybook2/"
 
 if [ -d ${DOXYBOOK2_OUT_DIR} ]; then
@@ -10,7 +12,7 @@ if [ -d ${DOXYBOOK2_OUT_DIR} ]; then
     rm -r ${DOXYBOOK2_OUT_DIR}/*
 fi
 
-#echo "generate doxybook files"
+echo "generating doxybook files"
 ${DOXYBOOK2_EXEC} --config config.json --input ${MURPHY_DOC_DIR}/xml --output ${DOXYBOOK2_OUT_DIR}
 
 
